303bd150e496b1ac07a65cd660d6870253e6d1e5
Sumit Rai <raisumitur@gmail.com>
2024-05-04 01:02:24 +0530

    UI

---
+++ src/main/java/com/example/chatroom/chat/MessageType.java
@@ -0,0 +1,8 @@
+package com.example.chatroom.chat;
+
+public enum MessageType {
+    CHAT,
+    JOIN,
+    LEAVE
+
+}
--- src/main/java/com/example/chatroom/config/WebSocketEventListener.java
+++ src/main/java/com/example/chatroom/config/WebSocketEventListener.java
@@ -1,9 +1,14 @@
 package com.example.chatroom.config;
 
+import com.example.chatroom.chat.ChatMessage;
+import com.example.chatroom.chat.MessageType;
 import jakarta.websocket.Session;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
@@ -12,10 +17,23 @@ import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 @Slf4j
 public class WebSocketEventListener {
 
+    private final SimpMessageSendingOperations messageTemplate;
+
     @EventListener
     public void handleWebSocketDisconnectListener(
             SessionDisconnectEvent event
     ){
         //Todo
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if(username != null){
+            log.info("User disconnected: {}", username);
+            var chatMessage = ChatMessage.builder()
+                    .type(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+
+            messageTemplate.convertAndSend("/topic/public",chatMessage);
+        }
     }
 }
